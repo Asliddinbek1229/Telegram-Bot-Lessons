d64d3ddf41910cad74c697699af18f7756a2493a
Asliddinbek1229 <asliddinbek1229@gmail.com>
2023-05-15 17:15:46 +0500

    The text formatting lesson is over. A new info_html file has been created

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,4 +2,5 @@ from . import setting
 from . import help
 from . import start
 from . import menuHandler
+from . import info_html
 from . import echo
---
+++ handlers/users/info_html.py
@@ -0,0 +1,17 @@
+from aiogram import types
+from states.personalData import PersonalData
+
+from loader import dp
+
+@dp.message_handler(commands='info_html')
+async def bot_help(message: types.Message):
+    text = (
+        f"<b>Assalomu alaykum {message.from_user.full_name}</b>\n"
+    )
+    text += ("<i>Bu egri matn</i>\n")
+    text += ("<u>Bu ostiga chizilgan matn</u>\n")
+    text += ("<s>Bu o'chirilgan matn</s>\n")
+    text += ("<code>print (\'Hello world\')</code> Bu kod format\n")
+    text += ("<a href=\'https://t.me/Asliddinbek_official\'> Asliddinbek Dagaraliyev</a>")
+    
+    await message.answer(text)
